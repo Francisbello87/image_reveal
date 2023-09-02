75b2940ce42a4e209d5e87813100bab03bd6efcc
Francis Bello <bellofrancis87@gmail.com>
2023-09-02 14:17:02 +0100

    fixing

--- app/page.js
+++ app/page.js
@@ -5,63 +5,64 @@ import { gsap } from "gsap/dist/gsap";
 
 export default function Home() {
   useEffect(() => {
-    const projects = document.querySelector(".projects");
-    const preview = document.querySelector(".preview");
-    const previewImg = document.querySelector(".preview-img");
+    if (projects && preview && previewImg) {
+      let isInside = false;
+      const bgPositions = {
+        p1: "0 0",
+        p2: "0 25%",
+        p3: "0 50%",
+        p4: "0 75%",
+        p5: "0 100%",
+      };
+      const moveStuff = (e) => {
+        const mouseInside = isMouseInsideContainer(e);
 
-    let isInside = false;
-    const bgPositions = {
-      p1: "0 0",
-      p2: "0 25%",
-      p3: "0 50%",
-      p4: "0 75%",
-      p5: "0 100%",
-    };
-    const moveStuff = (e) => {
-      const mouseInside = isMouseInsideContainer(e);
-
-      if (mouseInside !== isInside) {
-        isInside = mouseInside;
-        if (isInside) {
-          gsap.to(preview, { scale: 1 }, 0.3);
-        } else {
-          gsap.to(preview, { scale: 0 }, 0.3);
+        if (mouseInside !== isInside) {
+          isInside = mouseInside;
+          if (isInside) {
+            gsap.to(preview, { scale: 1 }, 0.3);
+          } else {
+            gsap.to(preview, { scale: 0 }, 0.3);
+          }
         }
-      }
-    };
+      };
 
-    const moveProject = (e) => {
-      const previewRect = preview.getBoundingClientRect();
-      const offsetX = previewRect.width / 2;
-      const offsetY = previewRect.height / 2;
+      const moveProject = (e) => {
+        const previewRect = preview.getBoundingClientRect();
+        const offsetX = previewRect.width / 2;
+        const offsetY = previewRect.height / 2;
 
-      preview.style.left = e.pageX - offsetX + "px";
-      preview.style.top = e.pageY - offsetY + "px";
-    };
+        preview.style.left = e.pageX - offsetX + "px";
+        preview.style.top = e.pageY - offsetY + "px";
+      };
 
-    const moveProjectImg = (project) => {
-      const projectId = project.id;
-      gsap.to(previewImg, 0.4, {
-        backgroundPosition: bgPositions[projectId] || "0 0",
+      const moveProjectImg = (project) => {
+        const projectId = project.id;
+        gsap.to(previewImg, 0.4, {
+          backgroundPosition: bgPositions[projectId] || "0 0",
+        });
+      };
+
+      const isMouseInsideContainer = (e) => {
+        const containerRect = projects.getBoundingClientRect();
+        return (
+          e.pageX >= containerRect.left &&
+          e.pageX <= containerRect.right &&
+          e.pageY >= containerRect.top &&
+          e.pageY <= containerRect.bottom
+        );
+      };
+
+      window.addEventListener("mousemove", moveStuff);
+
+      Array.from(projects.children).forEach((project) => {
+        project.addEventListener("mousemove", moveProject);
+        project.addEventListener(
+          "mousemove",
+          moveProjectImg.bind(null, project)
+        );
       });
-    };
-
-    const isMouseInsideContainer = (e) => {
-      const containerRect = projects.getBoundingClientRect();
-      return (
-        e.pageX >= containerRect.left &&
-        e.pageX <= containerRect.right &&
-        e.pageY >= containerRect.top &&
-        e.pageY <= containerRect.bottom
-      );
-    };
-
-    window.addEventListener("mousemove", moveStuff);
-
-    Array.from(projects.children).forEach((project) => {
-      project.addEventListener("mousemove", moveProject);
-      project.addEventListener("mousemove", moveProjectImg.bind(null, project));
-    });
+    }
   }, [preview, previewImg, projects]);
 
   return (
